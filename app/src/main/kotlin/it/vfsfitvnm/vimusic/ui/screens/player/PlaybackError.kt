26eb99eb2baab5c54167f55d814f7471e3245677
Daniel <daniel-sevillano@hotmail.com>
2025-07-14 14:49:55 +0200

    Replacing LocalClipboardManager with LocalClipboard

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/player/PlaybackError.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/player/PlaybackError.kt
@@ -1,5 +1,6 @@
 package it.vfsfitvnm.vimusic.ui.screens.player
 
+import android.content.ClipData
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
@@ -29,14 +30,15 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -47,6 +49,7 @@ import it.vfsfitvnm.vimusic.service.LoginRequiredException
 import it.vfsfitvnm.vimusic.service.PlayableFormatNotFoundException
 import it.vfsfitvnm.vimusic.service.UnplayableException
 import it.vfsfitvnm.vimusic.service.VideoIdMismatchException
+import kotlinx.coroutines.launch
 import java.net.UnknownHostException
 import java.nio.channels.UnresolvedAddressException
 
@@ -55,7 +58,8 @@ fun PlaybackError(
     error: PlaybackException?,
     onDismiss: () -> Unit,
 ) {
-    val clipboardManager = LocalClipboardManager.current
+    val clipboard = LocalClipboard.current
+    val scope = rememberCoroutineScope()
     var isShowingLogsDialog by rememberSaveable { mutableStateOf(false) }
 
     val errorMessage = when (error?.cause?.cause) {
@@ -120,6 +124,7 @@ fun PlaybackError(
 
     if (isShowingLogsDialog) {
         val errorText = Log.getStackTraceString(error)
+        val errorTitle = stringResource(id = R.string.error_log)
 
         AlertDialog(
             onDismissRequest = { isShowingLogsDialog = false },
@@ -132,13 +137,21 @@ fun PlaybackError(
             },
             dismissButton = {
                 TextButton(
-                    onClick = { clipboardManager.setText(AnnotatedString(errorText)) }
+                    onClick = {
+                        scope.launch {
+                            clipboard.setClipEntry(
+                                ClipEntry(
+                                    ClipData.newPlainText(errorTitle, errorText)
+                                )
+                            )
+                        }
+                    }
                 ) {
                     Text(text = stringResource(id = R.string.copy))
                 }
             },
             title = {
-                Text(text = stringResource(id = R.string.error_log))
+                Text(text = errorTitle)
             },
             text = {
                 Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
